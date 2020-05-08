0bfab9caa1e4bc52aee2e4da8c21998b65e5ecbf
Pascal Heitz <heitz.pascal@gmail.com>
2020-05-08 13:27:37 +0200

    The demo is getting somewhere

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,7 +1,7 @@
-import { createStyles, CssBaseline, makeStyles, Paper, TextField, Theme } from '@material-ui/core';
+import { createStyles, CssBaseline, makeStyles, Paper, TextField, Theme, Button, Checkbox, FormControlLabel } from '@material-ui/core';
 import { Field, Form, Formik } from 'formik'; // Using formik-undo's  formik module one folder up.
-import { FormikUndoContextProvider, FormikUndoControl, useFormikUndoAutoSave } from 'formik-undo';
-import React from 'react';
+import { FormikUndoContextProvider, FormikUndoControl, useFormikUndoAutoSave, useFormikUndo } from 'formik-undo';
+import React, { useState } from 'react';
 import { MaterialFormikUndoControl } from './MaterialUiFormikUndoControl';
 
 
@@ -21,34 +21,127 @@ const initialValues = {
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      display: 'flex',
+    },
+    main: {
       margin: theme.spacing(2),
       padding: theme.spacing(2),
+      flex: '1',
+    },
+    sidebar: {
+      margin: theme.spacing(2),
+      padding: theme.spacing(2),
+      flex: 0,
+    },
+    autoSaveControl: {
+      display: 'flex',
+      flexDirection: 'row',
+      marginBottom: theme.spacing(4),
+    },
+    autoSaveControlField: {
+      width: '5em',
+    },
+    undoableCounter: {
+      width: '5em',
     },
     undoControlBar: {
       display: 'flex',
+      alignItems: 'center',
       marginBottom: theme.spacing(4),
     },
     undoControlBarTitle: {
       display: 'block',
       width: '20em',
     },
+    vanillaControlBar: {
+      margin: theme.spacing(1),
+    }
   }),
 );
+
+
+const SaveCheckpointButton = () => {
+  const { saveCheckpoint } = useFormikUndo();
+  return (
+    <Button
+      variant="contained"
+      size="small"
+      onClick={saveCheckpoint}
+      title="Create a checkpoint now in the undo history"
+    >
+      Manual Save
+    </Button>
+  );
+};
+
+
+const UndoableCounter = () => {
+  const classes = useStyles();
+  const { undoableCount } = useFormikUndo();
+  return (
+    <TextField
+      className={classes.undoableCounter}
+      type="number"
+      disabled={true}
+      label="Undoable Count"
+      size="small"
+      value={undoableCount}
+    />
+  );
+};
+
+
+const AutoSaveControl = () => {
+  const classes = useStyles();
+  const [enabled, setEnabled] = useState(true);
+  const [throttleDelay, setThrottleDelay] = useState(2000);
+  const [debounceDelay, setDebounceDelay] = useState(500);
+  useFormikUndoAutoSave({ enabled, throttleDelay, debounceDelay });
+  return (
+    <div className={classes.autoSaveControl}>
+      <FormControlLabel
+        control={
+          <Checkbox checked={enabled} onChange={(e, checked) => setEnabled(checked)} />
+        }
+        label="AutoSave"
+      />
+      <TextField
+        className={classes.autoSaveControlField}
+        type="number"
+        label="Throttle"
+        size="small"
+        value={throttleDelay}
+        onChange={
+          (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setThrottleDelay(+e.target.value)
+        }
+      />
+    </div>
+  );
+};
+
+
+const Sidebar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AutoSaveControl />
+      <SaveCheckpointButton />
+      <UndoableCounter/>
+    </>
+  )
+};
 
 
 
 const MyForm = () => {
   const classes = useStyles();
-  useFormikUndoAutoSave(1000, 1000);
   return (
     <Form>
       <div className={classes.undoControlBar}>
-        <div className={classes.undoControlBarTitle}>Material UI</div>
         <MaterialFormikUndoControl />
-      </div>
-      <div className={classes.undoControlBar}>
-        <div className={classes.undoControlBarTitle}>Vanilla elements</div>
-        <FormikUndoControl />
+        <div>(or Vanilla elements</div>
+        <FormikUndoControl className={classes.vanillaControlBar} />
+        <div>)</div>
       </div>
       <Field as={TextField} name="title" />
       <Field as={TextField} name="content" />
@@ -65,19 +158,22 @@ const App = () => {
   };
 
   return (
-    <>
+    <div className={classes.root}>
       <CssBaseline />
-      <Paper className={classes.root}>
         <Formik
           initialValues={initialValues}
           onSubmit={handleSubmit}
         >
           <FormikUndoContextProvider>
-            <MyForm />
+            <Paper className={classes.main}>
+              <MyForm />
+            </Paper>
+            <Paper className={classes.sidebar}>
+              <Sidebar />
+            </Paper>
           </FormikUndoContextProvider>
         </Formik>
-      </Paper>
-    </>
+    </div>
   );
 };
 
--- src/useFormikUndoAutoSave.tsx
+++ src/useFormikUndoAutoSave.tsx
@@ -3,16 +3,35 @@ import { useFormikUndo } from './FormikUndo';
 import { useDebouncedValue, useEffectAfterFirstChange, useThrottledValue } from './hooks';
 
 
-export const useFormikUndoAutoSave = <T extends object>(throttleDelay: number, debounceDelay: number) => {
+interface AutoSaveConfig {
+  throttleDelay: number;
+  debounceDelay: number;
+  enabled: boolean;
+}
+
+type AutoSaveOptions = Partial<AutoSaveConfig>;
+
+const defaultOptions: AutoSaveConfig = {
+  throttleDelay: 2000,
+  debounceDelay: 500,
+  enabled: true,
+};
+
+
+export const useFormikUndoAutoSave = <T extends object>(options: AutoSaveOptions = {}) => {
+  const { throttleDelay, debounceDelay, enabled } = {...defaultOptions, ...options };
   const { values } = useFormikContext<T>();
   const { saveCheckpoint } = useFormikUndo();
   const throttledValue = useThrottledValue(values, throttleDelay);
   const throttledDebouncedValues = useDebouncedValue(throttledValue, debounceDelay);
   useEffectAfterFirstChange(
     () => {
+      if (!enabled) {
+        return;
+      }
       saveCheckpoint();
     },
     throttledDebouncedValues,
-    [],
+    [enabled],
   );
 };
